fb38364bd5aa0f81a6c82667d499631a332301ad
Lucas <lucasduartedeveloper@gmail.com>
2024-01-10 17:11:58 -0300

    first commit

--- script/plugin/video-reprojected_3.js
+++ script/plugin/video-reprojected_3.js
@@ -41,6 +41,74 @@ var setupCanvas = function() {
     pictureView.style.zIndex = "15";
     document.body.appendChild(pictureView);
 
+    switchSide = false;
+    switchSideView = document.createElement("button");
+    switchSideView.style.position = "absolute";
+    switchSideView.style.background = "#fff";
+    switchSideView.innerText = switchSide ? 
+    "camera | stream" : "stream | camera";
+    switchSideView.style.textAlign = "center";
+    switchSideView.style.left = ((sw/2)-50)+"px";
+    switchSideView.style.top = ((sh/2)-(sw/2)-35)+"px";
+    switchSideView.style.width = (100)+"px";
+    switchSideView.style.height = (25)+"px";
+    switchSideView.style.zIndex = "15";
+    document.body.appendChild(switchSideView);
+
+    switchSideView.onclick = function() {
+        switchSide = !switchSide;
+        if (switchSide) {
+            rotateStreamView.style.left = (sw-110)+"px";
+            rotateCameraView.style.left = (10)+"px";
+        }
+        else {
+            rotateStreamView.style.left = (10)+"px";
+            rotateCameraView.style.left = (sw-110)+"px";
+        }
+        switchSideView.innerText = switchSide ? 
+        "camera | stream" : "stream | camera";
+    };
+
+    rotateStream = false;
+    rotateStreamView = document.createElement("button");
+    rotateStreamView.style.position = "absolute";
+    rotateStreamView.style.background = "#fff";
+    rotateStreamView.innerText = rotateStream ? 
+    "stream: -180°" : "stream: 0°";
+    rotateStreamView.style.textAlign = "center";
+    rotateStreamView.style.left = (10)+"px";
+    rotateStreamView.style.top = ((sh/2)-(sw/2)-35)+"px";
+    rotateStreamView.style.width = (100)+"px";
+    rotateStreamView.style.height = (25)+"px";
+    rotateStreamView.style.zIndex = "15";
+    document.body.appendChild(rotateStreamView);
+
+    rotateStreamView.onclick = function() {
+        rotateStream = !rotateStream;
+        rotateStreamView.innerText = rotateStream ? 
+        "stream: -180°" : "stream: 0°";
+    }
+
+    rotateCamera = false;
+    rotateCameraView = document.createElement("button");
+    rotateCameraView.style.position = "absolute";
+    rotateCameraView.style.background = "#fff";
+    rotateCameraView.innerText = rotateCamera ? 
+    "camera: -180°" : "camera: 0°";
+    rotateCameraView.style.textAlign = "center";
+    rotateCameraView.style.left = (sw-110)+"px";
+    rotateCameraView.style.top = ((sh/2)-(sw/2)-35)+"px";
+    rotateCameraView.style.width = (100)+"px";
+    rotateCameraView.style.height = (25)+"px";
+    rotateCameraView.style.zIndex = "15";
+    document.body.appendChild(rotateCameraView);
+
+    rotateCameraView.onclick = function() {
+        rotateCamera = !rotateCamera;
+        rotateCameraView.innerText = rotateCamera ? 
+        "camera: -180°" : "camera: 0°";
+    };
+
     ontouch = false;
     startX = 0;
     startY = 0;
@@ -216,6 +284,11 @@ var drawImage = function() {
     resolutionCtx.imageSmoothingEnabled = false;
 
     resolutionCtx.save();
+    if (rotateStream) {
+        resolutionCtx.scale(-1, 1);
+        resolutionCtx.translate(-resolutionCanvas.width, 0);
+    }
+
     var image = {
         width: video.videoWidth,
         height: video.videoHeight
@@ -224,12 +297,20 @@ var drawImage = function() {
         width: (getSquare(video)/2),
         height: getSquare(video)
     };
+
+    var left = (switchSide && !rotateStream) || 
+    (!switchSide && rotateStream) ? 
+    (resolutionCanvas.width/2) : 0;
+
     var format = fitImageCover(image, frame);
     resolutionCtx.drawImage(video,
     -format.left, -format.top, frame.width, frame.height, 
-    0, 0, (resolutionCanvas.width/2), resolutionCanvas.height);
+    left, 0, (resolutionCanvas.width/2), resolutionCanvas.height);
 
-    if (objectPosition == 1) {
+    resolutionCtx.restore();
+
+    resolutionCtx.save();
+    if (rotateCamera) {
         resolutionCtx.scale(-1, 1);
         resolutionCtx.translate(-resolutionCanvas.width, 0);
     }
@@ -245,9 +326,13 @@ var drawImage = function() {
     var format = fitImageCover(image, frame);
     console.log(format);
 
+    var left = (switchSide && !rotateCamera) || 
+    (!switchSide && rotateCamera) ? 
+    0 : (resolutionCanvas.width/2);
+
     resolutionCtx.drawImage(cameraView,
     -format.left, -format.top, frame.width, frame.height, 
-    0, 0, 
+    left, 0, 
     (resolutionCanvas.width/2), 
     resolutionCanvas.height);
 
